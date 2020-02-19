1e8f63f1d1caeabb9d1428696e96cc27e61d7c6b
funayu <funayu0908@gmail.com>
2020-02-19 17:48:01 +0900

    ES2015のプリミティブ

---
+++ 16-18-1.js
@@ -0,0 +1,3 @@
+console.log(0b1010);
+console.log(0o755);
+console.log(2 ** 3);
---
+++ 16-18-2.js
@@ -0,0 +1,3 @@
+console.log(Number.isInteger(3));
+console.log(Number.isInteger(2.5));
+console.log(Number.isInteger(2 ** 128));
